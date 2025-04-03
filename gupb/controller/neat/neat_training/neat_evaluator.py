d58c000ae8be381a46c232d91d79454893a49f8c
Radosław Mocarski <mocarski@student.agh.edu.pl>
2025-04-03 20:52:45 +0200

    ewaluator + aktualizacja fitness co turę

--- gupb/controller/neat/neat_training/neat_evaluator.py
+++ gupb/controller/neat/neat_training/neat_evaluator.py
@@ -4,22 +4,79 @@ from typing_extensions import override
 
 from gupb import runner
 from gupb.controller.neat.kim_dzong_neat_jr import KimDzongNeatJuniorController
+from gupb.model import games, weapons
 
 
 class NeatEvaluator:
-    def __init__(self, controller: KimDzongNeatJuniorController, runner: runner.Runner):
+    def __init__(self, controller: KimDzongNeatJuniorController, runner: runner.Runner, cumulative: bool):
         self.controller = controller
         self.runner = runner
+        self.cumulative = cumulative
 
     @abstractmethod
-    def calculate_score(self):
+    def calculate_score(self,game: games.Game=None, tick_count: int=0):
         raise NotImplementedError("This method must be implemented in a subclass")
 
 
 class NeatEvaluatorV1(NeatEvaluator):
-    def __init__(self, controller: KimDzongNeatJuniorController, runner: runner.Runner):
+    def __init__(self, controller: KimDzongNeatJuniorController, runner: runner.Runner, cumulative: bool):
         super().__init__(controller, runner)
 
     @override
-    def calculate_score(self):
+    def calculate_score(self,game: games.Game=None, tick_count: int=0):
         return self.runner.scores["Kim Dzong Neat v_1"]
+    
+
+class NeatEvaluatorVT1(NeatEvaluator):
+    def __init__(self, controller: KimDzongNeatJuniorController, runner: runner.Runner, cumulative: bool):
+        super().__init__(controller, runner, cumulative)
+        self.bonuses = {
+            'survival': 0.2,
+            'health': 0.5,
+            'enemy': -0.05,
+            'death': -150,
+            'weapon': 0.1,
+            'knife': 0,
+            'sword': 0.05,
+            'axe': 0.06,
+            'bow':0.2,
+            'amulet':0.06,
+            'scroll':0.06,
+        }
+        self.was_death = False
+
+    @override
+    def calculate_score(self, game: games.Game, tick_count: int):
+        current_score = 0
+        neat_controller = self.controller
+        bonuses = self.bonuses
+        cumulative = self.cumulative
+
+        for champion in game.champions:
+            if champion.controller == neat_controller:
+                self.was_death = False
+                if cumulative:
+                    current_score += tick_count * bonuses['survival']
+                else:
+                    current_score += bonuses['survival']
+                current_score += champion.health * bonuses['health']
+                if hasattr(champion, 'weapon') and champion.weapon:
+                    current_score += bonuses['weapon'] 
+                    if isinstance(champion.weapon, weapons.Knife):
+                        current_score += bonuses['knife']
+                    elif isinstance(champion.weapon, weapons.Sword):
+                        current_score += bonuses['sword']
+                    elif isinstance(champion.weapon, weapons.Axe):
+                        current_score += bonuses['axe']
+                    elif isinstance(champion.weapon, weapons.Bow):
+                        current_score += bonuses['bow']
+                    elif isinstance(champion.weapon, weapons.Amulet):
+                        current_score += bonuses['amulet']
+                    elif isinstance(champion.weapon, weapons.Scroll):
+                        current_score += bonuses['scroll']
+            else:
+                current_score += bonuses['enemy']
+        if (not any(champion.controller == neat_controller for champion in game.champions)) and (not self.was_death):
+            self.was_death = True
+            current_score += bonuses['death']
+        return current_score
--- gupb/controller/neat/neat_training/train_neat.py
+++ gupb/controller/neat/neat_training/train_neat.py
@@ -4,6 +4,7 @@ from gupb import runner
 from gupb.controller import random
 from gupb.controller.neat.kim_dzong_neat_jr import KimDzongNeatJuniorController
 from gupb.controller.neat.neat_training.neat_evaluator import NeatEvaluatorV1, NeatEvaluator
+from gupb.model import games
 
 
 def default_game_configuration(controller: KimDzongNeatJuniorController):
@@ -50,12 +51,49 @@ def eval_genomes_with_evaluator(evaluator_name, genomes, config):
 
         genome.fitness += evaluator.calculate_score()
 
+def eval_genomes_with_by_tick(evaluator_name, genomes, config, ticks_per_update=1):
+    evaluator = get_evaluator(evaluator_name, neat_controller, game_runner)
+    for genome_id, genome in genomes:
+        genome.fitness = 0
+        net = neat.nn.FeedForwardNetwork.create(genome, config)
+        neat_controller = KimDzongNeatJuniorController(net=net)
 
-def run_neat_training(config: neat.Config, n: int, evaluator_name: str):
+        game_config = default_game_configuration(neat_controller)
+        game_runner = runner.Runner(game_config)
+        
+        arena = random.choice(game_runner.arenas)
+        game = games.Game(
+            game_no=0,
+            arena_name=arena,
+            to_spawn=game_runner.controllers,
+        )
+        tick_count = 0
+        
+        play = True
+        while play:
+            game.cycle()
+            tick_count += 1
+            if game.finished:
+                play = False
+            if tick_count % ticks_per_update == 0 or not play:
+                game_runner._last_arena = game.arena.name
+                game_runner._last_menhir_position = game.arena.menhir_position
+                game_runner._last_initial_positions = game.initial_champion_positions
+                score = evaluator.calculate_score(game)
+                genome.fitness += score
+        game_runner._last_arena = game.arena.name
+        game_runner._last_menhir_position = game.arena.menhir_position
+        game_runner._last_initial_positions = game.initial_champion_positions
+
+
+def run_neat_training(config: neat.Config, n: int, evaluator_name: str, turns_to_update: int):
     population = neat.Population(config)
     population.add_reporter(neat.StdOutReporter(True))
     population.add_reporter(neat.StatisticsReporter())
 
-    eval_genomes = lambda genomes, neat_config: eval_genomes_with_evaluator(evaluator_name, genomes, neat_config)
+    if turns_to_update < 1:
+        eval_genomes = lambda genomes, neat_config: eval_genomes_with_evaluator(evaluator_name, genomes, neat_config)
+    else:
+        eval_genomes = lambda genomes, neat_config: eval_genomes_with_by_tick(evaluator_name, genomes, neat_config, turns_to_update)
     winner = population.run(eval_genomes, n)
     return winner
